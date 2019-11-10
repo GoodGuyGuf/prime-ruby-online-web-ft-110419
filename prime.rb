f0c2af91864cc43bff3e7096850320d7732d4a68
Ethan Gustafson <ethan_gustafson@outlook.com>
2019-11-10 22:31:57 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,6 @@
 # Add  code here!
 require 'prime'
 def prime?(integer)
-  prime = true
 integer = []
-integer.any? do |number| if number.prime?
-else
-  prime = false
+integer.any?{|number| number.prime?}
 end
