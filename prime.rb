c8f773aa890e35e7eb08ed5fd5359d7a108783e6
Ethan Gustafson <ethan_gustafson@outlook.com>
2019-11-10 22:15:37 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,8 @@
 # Add  code here!
 require 'prime'
 def prime?(integer)
-prime = true
-integer.each do |number|
-  if number.prime?
-  prime = false
-end
-end
+integer = []
+integer.all?{|number| number == number.prime?}
+
+
 end
