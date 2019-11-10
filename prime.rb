8849a86c6e2bdc2a88664d00cc60a4828cde3226
Ethan Gustafson <ethan_gustafson@outlook.com>
2019-11-10 23:06:23 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,3 @@
-def prime?(int)
-  if int > 1
-    (2..int-1).none? { |i| int%i == 0 }
-  else
-    false
-  end
-end
-
 def prime?(integer)
   if integer > 1
   i(2..int-1).none? {|number| integer%number == 0}
